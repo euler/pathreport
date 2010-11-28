8485abc355b22dcb1298e911292e0cf90bc05df9
Ray Niemeir <ray.niemeir@gmail.com>
2010-11-27 21:57:33 -0700

    Cleanups.

--- pathtest.sh
+++ pathtest.sh
@@ -2,17 +2,28 @@
 #
 # Help figure out PATH settings
 #
+
+# Emit break between output sections 
 br() {
   echo "-----"
 }
+
+# Show path elements as separate lines
 aslines() {
   echo $PATH | tr : '\n'
 }
+
+# Show only live (existing) paths
 livepaths() {
 for f in `aslines`
 do
   [[ -d $f ]] && echo $f
 done
+}
+
+# Show all files reachable on PATH directories
+pathfiles() {
+  find `livepaths` -type f -maxdepth 1
 }
 
 echo 'echo $PATH'
@@ -27,22 +38,22 @@ br
 echo "Issues: "
 for f in `aslines`
 do
-  [[ ! -d $f ]] && echo "Not a directory, ignore: " $f
+  [[ ! -d $f ]] && echo "Not a directory, ignoring: " $f
 done
 br
 
-echo '#Bytes'	"	" "In path"
+echo '#Bytes'	"	" "In PATH tree"
 du -hs `livepaths`
 br
 
-  echo '#Files'	"	" "In path"
+echo '#Files'	"	" "In PATH directory"
 for f in `livepaths`
 do
   echo $(find $f -type f -maxdepth 1 | wc -l) "	" $f
 done
 br
 echo "Files duplicated in PATH"
-find `livepaths` -type f | sed -e 's=^.*/==' | sort | uniq -c | sed -e '/^ *1 /d' | sort -n
+pathfiles | sed -e 's=^.*/==' | sort | uniq -c | sed -e '/^ *1 /d' | sort -n
 
 br
 # 1 == "previous", 2 == "current", 3 == "next"
@@ -51,21 +62,18 @@ cmd1="dummy 1";path1=""
 cmd2="dummy 2";path2=""
 cmd3="dummy 3";path3=""
 echo "Duplicate definitions in PATH"
-find `livepaths` -type f | tr ' ' _ | sed -e 's=\(^.*/\)=\1 =' |
+pathfiles | tr ' ' _ | sed -e 's=\(^.*/\)=\1 =' |
 sort -k 2 | 
 rev |
- while read cmd3 path3  # read the new "next" command and path
+while read cmd3 path3  # read the new "next" command and path
  do
    # Emit the current line if it matches either the previous or the next (or both)
    if [[ ( "$cmd2" == "$cmd1" ) || ( "$cmd2" == "$cmd3" ) ]]
    then
      echo $cmd2 $path2
    fi
-   # Move current to previous and next to current
+   # Move current to previous and new next to current for next loop
    cmd1=$cmd2; path1=$path2
    cmd2=$cmd3; path2=$path3
  done | rev
  # Exiting the while loop with no "next". The old "current" was processed
-exit
-uniq -f 1 -d |
- sed -e '/^ *1 /d' | sort -n
