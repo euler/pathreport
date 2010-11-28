325badd087bdd3850fcd7feabc639a2220000f95
Ray Niemeir <ray.niemeir@gmail.com>
2010-11-27 21:38:13 -0700

    Basics working.

--- pathtest.sh
+++ pathtest.sh
@@ -38,8 +38,34 @@ br
   echo '#Files'	"	" "In path"
 for f in `livepaths`
 do
-  echo $(find $f | wc -l) "	" $f
+  echo $(find $f -type f -maxdepth 1 | wc -l) "	" $f
 done
 br
-echo "Duplicated in PATH"
-find `livepaths` | sed -e 's=^.*/==' | sort | uniq -c | sed -e '/^ *1 /d' | sort -n
+echo "Files duplicated in PATH"
+find `livepaths` -type f | sed -e 's=^.*/==' | sort | uniq -c | sed -e '/^ *1 /d' | sort -n
+
+br
+# 1 == "previous", 2 == "current", 3 == "next"
+# Initialize everything so we can start reading "next"
+cmd1="dummy 1";path1=""
+cmd2="dummy 2";path2=""
+cmd3="dummy 3";path3=""
+echo "Duplicate definitions in PATH"
+find `livepaths` -type f | tr ' ' _ | sed -e 's=\(^.*/\)=\1 =' |
+sort -k 2 | 
+rev |
+ while read cmd3 path3  # read the new "next" command and path
+ do
+   # Emit the current line if it matches either the previous or the next (or both)
+   if [[ ( "$cmd2" == "$cmd1" ) || ( "$cmd2" == "$cmd3" ) ]]
+   then
+     echo $cmd2 $path2
+   fi
+   # Move current to previous and next to current
+   cmd1=$cmd2; path1=$path2
+   cmd2=$cmd3; path2=$path3
+ done | rev
+ # Exiting the while loop with no "next". The old "current" was processed
+exit
+uniq -f 1 -d |
+ sed -e '/^ *1 /d' | sort -n
