cf17673913c056c4c0d6cca2296cd21dc79081e0
Ray Niemeir <ray.niemeir@gmail.com>
2010-12-05 19:11:44 -0700

    Refactor "cmd" to "file" state variable names.  Improve comments.

--- pathtest.sh
+++ pathtest.sh
@@ -67,36 +67,43 @@ echo "#copies"
 pathfiles | sed -e 's=^.*/==' | sort | uniq -c | sed -e '/^ *1 /d' | sort -nr
 br
 
-# 1 == "previous", 2 == "current", 3 == "next"
-# Initialize everything to unmatchable values so we can start reading "next"
-cmd1="dummy 1";path1=""
-cmd2="dummy 2";path2=""
-cmd3="dummy 3";path3=""
+# Prepare to loop through the set of all reachable filenames (commands) sorted by
+# filename.  Report all the duplicate paths for each filename.
+#
+# Initialize state variables to unmatchable values.
+# This will let us smoothly enter the "read nextine" loop below with coherent state.
+# The state variables are
+#    1 == "previous", 2 == "current", 3 == "next" for filenames and paths
+file1="dummy 1 /previous"; path1=""
+file2="dummy 2 /current";  path2=""
+file3="dummy 3 /next";     path3=""
 
-echo "Pathnames of the duplicate definitions"
+echo "Pathnames of the duplicate filenames"
 pathfiles |
 while read fullpath
 do
-  [[ $fullpath =~ /([^/]*)$ ]]
   # Emit "basename|fullpath". The "|" is a delimiter not in filenames.
+  [[ $fullpath =~ /([^/]*)$ ]]
   echo ${BASH_REMATCH[1]}'|'$fullpath
 done |
 sort |  # by basename, with fullpath attached
-( cat ; echo "dummy last|none" ) |  # append a dummy to end of sorted stream
-while read nextline  # the new next "command|fullpath"
+( cat ; echo "dummy last|none" ) |  # append a dummy to end of sorted stream for smooth finish
+while read nextline  # the new next "filename|fullpath"
  do
-   # Separate the sorted record back into command and fullpath
+   # Enter here with "filename|fullpath" records sorted by ascending filename
+   # Split the sorted record on '|' back into filename and fullpath
    [[ $nextline =~ ^([^|]*)\|(.*)$ ]]
-   cmd3=${BASH_REMATCH[1]}
+   # file3 and path3 hold the "next" valuses
+   file3=${BASH_REMATCH[1]}
    path3=${BASH_REMATCH[2]}
 
-   # Emit the full path for "current" cmd2 if it  matches
-   # either the previous or the next cmd (or both)
-   [[ ("$cmd2" == "$cmd1") || ("$cmd2" == "$cmd3") ]] && echo $path2
+   # Emit the full path for "current" file2 if it  matches
+   # either the previous or the next file (or both)
+   [[ ("$file2" == "$file1") || ("$file2" == "$file3") ]] && echo $path2
 
-   # Move current to previous and new next to current for next loop
-   cmd1=$cmd2; # path1=$path2
-   cmd2=$cmd3; path2=$path3
+   # Move current to previous and next to current
+   file1=$file2; # path1=$path2
+   file2=$file3; path2=$path3
  done 
  # Exit the while loop with no more "next". The old "current" was processed
 
